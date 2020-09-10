03f22b6d0d1cd33e857ac3dafe99a50e20ec7c7c
Alex Bettadapur <alexbettadapur@gmail.com>
2020-09-09 17:11:00 -0700

    Initial commit

---
+++ not-working/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    devtool:'source-map',
+    entry:  {"index": "./src/index.ts"},
+    module: {
+        rules: [
+            { test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              inlineSourceMap: false,
+              sourceMap: true,
+              module: 'es6', // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+            },
+          },
+        },
+      ],
+      exclude: /(node_modules|__tests__|__mocks__)/,
+    }
+        ]
+    },
+      "resolve": {
+    "extensions": [
+      ".tsx",
+      ".ts",
+      ".js",
+      ".jsx",
+      ".json"
+    ],
+      },
+}
---
+++ working/src/index.ts
@@ -0,0 +1,4 @@
+import { foo } from "./foo";
+
+console.log("a_b");
+foo();
